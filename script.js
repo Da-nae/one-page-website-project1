ddf1436c82200e815f09e09372f65e36de829e47
Danaé Grosjean <105633406+Da-nae@users.noreply.github.com>
2022-06-02 12:31:28 +0200

    Adding the updated JS file

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 
 let x = document.querySelector("img#logo-header")
+let y = document.querySelector("link[rel~='icon']");
 let i = 0;
 
 x.addEventListener("click", function() {
     i++
     if(i == 1) {
         x.src = "logo2.png"
+        y.href = "favicon2.ico"
     }
     if(i == 2) {
         x.src = "logo3.png"
+        y.href = "favicon3.ico"
     }
     if(i == 3) {
         x.src = "logo.png"
+        y.href = "favicon.ico"
         i = 0
     }
 })
