799eac89b2040ee5c11272635054114d896aa6ca
Danaé Grosjean <105633406+Da-nae@users.noreply.github.com>
2022-06-02 14:14:49 +0200

    Add JS easter egg + fixed a width problem on desktop

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
-let x = document.querySelector("img#logo-header")
+let x = document.querySelector('img#logo-header');
 let y = document.querySelector("link[rel~='icon']");
+let z = document.querySelector('img#logo-footer');
 let i = 0;
 
 x.addEventListener("click", function() {
@@ -12,10 +13,50 @@ x.addEventListener("click", function() {
     if(i == 2) {
         x.src = "logo3.png"
         y.href = "favicon3.ico"
+        z.src = "logo3.png"
     }
     if(i == 3) {
+        x.src = "logo4.png"
+        y.href = "favicon4.ico"
+        z.src = "logo4.png"
+    }
+    if(i == 4) {
+        x.src = "logo5.png"
+        y.href = "favicon5.ico"
+        z.src = "logo5.png"
+    }
+    if(i == 5) {
+        x.src = "logo6.png"
+        y.href = "favicon6.ico"
+        z.src = "logo6.png"
+    }
+    if(i == 6) {
         x.src = "logo.png"
         y.href = "favicon.ico"
+        z.src = "logo.png"
+        i = 0
+    }
+})
+
+z.addEventListener("click", function() {
+    i++
+    if(i == 1) {
+        z.src = "logo2.png"
+    }
+    if(i == 2) {
+        z.src = "logo3.png"
+    }
+    if(i == 3) {
+        z.src = "logo4.png"
+    }
+    if(i == 4) {
+        z.src = "logo5.png"
+    }
+    if(i == 5) {
+        z.src = "logo6.png"
+    }
+    if (i == 6) {
+        z.src = "logo.png"
         i = 0
     }
 })
